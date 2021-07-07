352bea00cd491ddc10b462f52f073ee286da180e
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-07-07 11:44:05 +0300

    Add main menu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,30 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        boolean go = true;
+        System.out.println("Please select a number to select an option: ");
+        System.out.println("1. Recipes");
+        System.out.println("2. Ingredients");
+        System.out.println("3. Exit");
+        while (go) {
+            int number = scanner.nextInt();
+            switch (number) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    go = false;
+                    break;
+                default:
+                    System.out.println("Wrong number");
+                    System.out.println("Enter number from 1 to 4, please");
+
+            }
+
+   /*     Scanner scanner = new Scanner(System.in);
         boolean go = true;
 
         System.out.println("Choose number to do operation, please: ");
@@ -36,59 +59,60 @@ public class App {
                     System.out.println("Enter number from 1 to 4, please");
 
             }
+        }*/
         }
     }
 
-    //дописать всё
-    private static void findRecipeByIngredientsSet() {
-    }
+        //дописать всё
+        private static void findRecipeByIngredientsSet () {
+        }
 
 
-    private static void findRecipeByName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter recipe's name, please: ");
-        String name = scanner.nextLine();
+        private static void findRecipeByName () {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter recipe's name, please: ");
+            String name = scanner.nextLine();
 
-        // проверка на null
-        try {
-            if () {
+            // проверка на null
+            try {
+                if () {
 
-            } else {
-                System.out.println("This name doesn't exist");
+                } else {
+                    System.out.println("This name doesn't exist");
+                }
+            } catch (NullPointerException e) {
+                System.out.println("There is no name");
             }
-        } catch (NullPointerException e) {
-            System.out.println("There is no name");
-        }
-        System.out.println("You can rewrite description or delete some ingredients");
-        System.out.println("1. Rewrite descriptions ");
-        System.out.println("2. Delete ingredients ");
-        System.out.println("3. Exit ");
-        boolean go = true;
-        while (go) {
-            int number = scanner.nextInt();
-            switch (number) {
-                case 1:
+            System.out.println("You can edit description or delete some ingredients");
+            System.out.println("1. Edit descriptions ");
+            System.out.println("2. Delete ingredients ");
+            System.out.println("3. Back ");
+            boolean go = true;
+            while (go) {
+                int number = scanner.nextInt();
+                switch (number) {
+                    case 1:
 
-                    break;
-                case 2:
+                        break;
+                    case 2:
 
-                    break;
-                case 3:
-                    go = false;
-                    break;
+                        break;
+                    case 3:
+                        go = false;
+                        break;
 
-                default:
-                    System.out.println("Wrong number");
-                    System.out.println("Enter number from 1 to 3, please");
+                    default:
+                        System.out.println("Wrong number");
+                        System.out.println("Enter number from 1 to 3, please");
 
+                }
             }
         }
-    }
 
-    private static void showAllRecipes() {
-        List<Recipe> recipes; // =
-        System.out.println("List of all recipes: ");
-        recipes.forEach(recipe1 -> System.out.println(recipe1.toString()));
-    }
+        private static void showAllRecipes () {
+            List<Recipe> recipes; // =
+            System.out.println("List of all recipes: ");
+            recipes.forEach(recipe1 -> System.out.println(recipe1.toString()));
+        }
 
-}
+    }
