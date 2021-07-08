62b953734b8be81b651fd3091be2418da001f838
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-07-08 11:44:18 +0300

    Delete not used imports

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,7 @@
 package org.example;
 
-
-import org.example.entity.Recipe;
-import org.example.service.impl.RecipeServiceImpl;
 import org.example.view.MainView;
-
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws SQLException {
