3a87e43f3698b09d6234591e8521f87c13f6505d
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-07-08 15:21:45 +0300

    Add repository and implementation (not complete) for ingredient

---
+++ src/main/java/org/example/repository/IngredientRepository.java
@@ -0,0 +1,6 @@
+package org.example.repository;
+
+import org.example.entity.Ingredient;
+
+public interface IngredientRepository extends GenericRepository <Ingredient, Integer>{
+}
---
+++ src/main/java/org/example/repository/impl/IngredientRepoImpl.java
@@ -0,0 +1,63 @@
+package org.example.repository.impl;
+
+import org.example.entity.Ingredient;
+import org.example.entity.Recipe;
+import org.example.repository.IngredientRepository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IngredientRepoImpl implements IngredientRepository {
+
+    Connection connection;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+    @Override
+    public Ingredient save(Ingredient ingredient) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Ingredient update(Ingredient ingredient) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Ingredient> getAll() throws SQLException {
+        List<Ingredient> ingredients = new ArrayList<Ingredient>();
+        String query = "SELECT * FROM ingredients";
+        preparedStatement = connection.prepareStatement(query);
+        resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            Ingredient ingredient = new Ingredient(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getFloat("calories"));
+            ingredients.add(ingredient);
+        }
+        return ingredients;
+    }
+
+    @Override
+    public Ingredient getById(Integer integer) throws SQLException {
+        String query = "SELECT * FROM ingredient WHERE id = ?";
+
+        Ingredient ingredient = null;
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, id);
+        resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            Ingredient newIngredient = new Ingredient(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getFloat("calories"));
+            ingredient.setId(id);
+        }
+        return ingredient;
+    }
+
+    @Override
+    public void deleteById(Integer integer) throws SQLException {
+
+    }
+}
