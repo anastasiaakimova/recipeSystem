fb08d723d0b74b2449a2a0200d453ad7e1fddf67
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-07-12 13:02:50 +0300

    Add save method

--- src/main/java/org/example/repository/impl/IngredientRepoImpl.java
+++ src/main/java/org/example/repository/impl/IngredientRepoImpl.java
@@ -1,7 +1,6 @@
 package org.example.repository.impl;
 
 import org.example.entity.Ingredient;
-import org.example.entity.Recipe;
 import org.example.repository.IngredientRepository;
 
 import java.sql.Connection;
@@ -19,7 +18,15 @@ public class IngredientRepoImpl implements IngredientRepository {
 
     @Override
     public Ingredient save(Ingredient ingredient) throws SQLException {
-        return null;
+        String query = "INSERT INTO ingredient (id, name, calories) VALUES (?, ?, ?)";
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, ingredient.getId());
+        preparedStatement.setString(2, ingredient.getName());
+        preparedStatement.setFloat(3, ingredient.getCalories());
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+        connection.close();
+        return ingredient;
     }
 
     @Override
@@ -30,19 +37,21 @@ public class IngredientRepoImpl implements IngredientRepository {
     @Override
     public List<Ingredient> getAll() throws SQLException {
         List<Ingredient> ingredients = new ArrayList<Ingredient>();
-        String query = "SELECT * FROM ingredients";
+        String query = "SELECT * FROM ingredient";
         preparedStatement = connection.prepareStatement(query);
         resultSet = preparedStatement.executeQuery();
         while (resultSet.next()) {
             Ingredient ingredient = new Ingredient(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getFloat("calories"));
             ingredients.add(ingredient);
         }
+        preparedStatement.close();
+        connection.close();
         return ingredients;
     }
 
     @Override
-    public Ingredient getById(Integer integer) throws SQLException {
-        String query = "SELECT * FROM ingredients WHERE id = ?";
+    public Ingredient getById(Integer id) throws SQLException {
+        String query = "SELECT * FROM ingredient WHERE id = ?";
 
         Ingredient ingredient = null;
         preparedStatement = connection.prepareStatement(query);
@@ -51,13 +60,13 @@ public class IngredientRepoImpl implements IngredientRepository {
 
         while (resultSet.next()) {
             Ingredient newIngredient = new Ingredient(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getFloat("calories"));
-            ingredient.setId(id);
+            //     ingredient.setId(id);
         }
         return ingredient;
     }
 
     @Override
-    public void deleteById(Integer integer) throws SQLException {
+    public void deleteById(Integer id) throws SQLException {
 
     }
 }
