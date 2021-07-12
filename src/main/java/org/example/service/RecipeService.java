4979cf09094da96a9983b1c33f7131dade7a0a66
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-07-12 15:15:34 +0300

    Delete not used imports

--- src/main/java/org/example/service/RecipeService.java
+++ src/main/java/org/example/service/RecipeService.java
@@ -4,11 +4,9 @@ import org.example.entity.Ingredient;
 import org.example.entity.Recipe;
 
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Set;
 
 public interface RecipeService extends GenericService <Recipe, Integer>{
     Recipe findByName(String name) throws SQLException;
     Recipe findByIngredientsSet(Set<Ingredient> ingredient) throws  SQLException;
-
 }
