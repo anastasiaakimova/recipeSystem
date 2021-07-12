79151b0b374b054599573769a4b3bd8132c965ac
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-07-12 10:08:54 +0300

    Add database script

---
+++ src/main/resources/db/script.sql
@@ -0,0 +1,33 @@
+CREATE TABLE recipe
+(
+    id serial NOT NULL,
+    name character varying,
+    description character varying,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ingredient
+(
+    id serial NOT NULL,
+    name character varying,
+    calories real,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE recipe_ingredient
+(
+    id serial NOT NULL,
+    "idRecipe" integer,
+    "idIngredient" integer,
+    PRIMARY KEY (id),
+    CONSTRAINT "idRecipe" FOREIGN KEY ("idRecipe")
+        REFERENCES recipe (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT "idIngredient" FOREIGN KEY ("idIngredient")
+        REFERENCES ingredient (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
