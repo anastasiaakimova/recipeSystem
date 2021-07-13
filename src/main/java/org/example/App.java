9b2a4ae37a7e5459c64e6df58ba976a81b69bff0
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-07-13 13:08:44 +0300

    Delete db connection in the start of program

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,11 @@
 package org.example;
 
-import org.example.util.DbConnection;
 import org.example.view.MainView;
 
 import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        DbConnection dbConnection = new DbConnection();
-        dbConnection.getConnection();
         MainView.getInstance().run();
     }
 }
