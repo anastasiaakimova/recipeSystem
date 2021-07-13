d36c04783bddd9d5dcb2fecb5fb3634a10ad58c4
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-07-13 09:25:11 +0300

    Edit method update, replace name with id and update all other methods with connections

--- src/main/java/org/example/repository/impl/IngredientRepoImpl.java
+++ src/main/java/org/example/repository/impl/IngredientRepoImpl.java
@@ -2,6 +2,7 @@ package org.example.repository.impl;
 
 import org.example.entity.Ingredient;
 import org.example.repository.IngredientRepository;
+import org.example.util.DbConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,32 +13,29 @@ import java.util.List;
 
 public class IngredientRepoImpl implements IngredientRepository {
 
-    Connection connection;
-    PreparedStatement preparedStatement = null;
-    ResultSet resultSet = null;
-
     // метод добавляет новый ингредиент
     @Override
     public Ingredient save(Ingredient ingredient) throws SQLException {
-        String query = "INSERT INTO ingredient (id, name, calories) VALUES (?, ?, ?)";
-        preparedStatement = connection.prepareStatement(query);
-        preparedStatement.setInt(1, ingredient.getId());
-        preparedStatement.setString(2, ingredient.getName());
-        preparedStatement.setFloat(3, ingredient.getCalories());
+        Connection connection = DbConnection.getConnection();
+        String query = "INSERT INTO ingredient (name, calories) VALUES ( ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, ingredient.getName());
+        preparedStatement.setFloat(2, ingredient.getCalories());
         preparedStatement.executeUpdate();
         preparedStatement.close();
         connection.close();
         return ingredient;
     }
 
-    // метод изменяет поля (имя и калории) ингридиента по заданному id
+    // метод изменяет поля (имя и калории) ингридиента по заданному name
     @Override
     public Ingredient update(Ingredient ingredient) throws SQLException {
-        String query = "UPDATE ingredient SET name = ?, calories = ? WHERE id = ?";
-        preparedStatement = connection.prepareStatement(query);
+        Connection connection = DbConnection.getConnection();
+        String query = "UPDATE ingredient SET name = ?, calories = ? WHERE name = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
         preparedStatement.setString(1, ingredient.getName());
         preparedStatement.setFloat(2, ingredient.getCalories());
-        preparedStatement.setInt(3, ingredient.getId());
+        preparedStatement.setString(3, ingredient.getName());
         preparedStatement.executeUpdate();
         preparedStatement.close();
         connection.close();
@@ -47,10 +45,11 @@ public class IngredientRepoImpl implements IngredientRepository {
     // метод выводит все поля всех рецептов
     @Override
     public List<Ingredient> getAll() throws SQLException {
-        List<Ingredient> ingredients = new ArrayList<Ingredient>();
+        Connection connection = DbConnection.getConnection();
+        List<Ingredient> ingredients = new ArrayList<>();
         String query = "SELECT * FROM ingredient";
-        preparedStatement = connection.prepareStatement(query);
-        resultSet = preparedStatement.executeQuery();
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
         while (resultSet.next()) {
             Ingredient ingredient = new Ingredient(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getFloat("calories"));
             ingredients.add(ingredient);
@@ -63,12 +62,12 @@ public class IngredientRepoImpl implements IngredientRepository {
     // метод выводит все поля ингредиента по заданному id
     @Override
     public Ingredient getById(Integer id) throws SQLException {
+        Connection connection = DbConnection.getConnection();
         String query = "SELECT * FROM ingredient WHERE id = ?";
-
         Ingredient ingredient = new Ingredient();
-        preparedStatement = connection.prepareStatement(query);
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
         preparedStatement.setInt(1, id);
-        resultSet = preparedStatement.executeQuery();
+        ResultSet resultSet = preparedStatement.executeQuery();
 
         while (resultSet.next()) {
             Ingredient newIngredient = new Ingredient(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getFloat("calories"));
@@ -80,12 +79,12 @@ public class IngredientRepoImpl implements IngredientRepository {
         return ingredient;
     }
 
-    // удаление ингредиента
-
+    // удаление ингредиента по заданному name
     @Override
     public void deleteByName(String name) throws SQLException {
+        Connection connection = DbConnection.getConnection();
         String query = "DELETE * FROM ingredient WHERE name = ?";
-        preparedStatement = connection.prepareStatement(query);
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
         preparedStatement.setString(1, name);
         preparedStatement.executeUpdate();
         preparedStatement.close();
